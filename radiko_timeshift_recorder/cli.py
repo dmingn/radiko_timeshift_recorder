48b48d0096da9dd653e570300ed5cda7bdf87ec5
dmingn <dmingn@users.noreply.github.com>
2022-04-12 00:40:43 +0900

    refactor (#10)

--- radiko_timeshift_recorder/cli.py
+++ radiko_timeshift_recorder/cli.py
@@ -1,5 +1,7 @@
 import datetime
+import json
 import os
+import subprocess
 import time
 from pathlib import Path
 from typing import Optional
@@ -10,7 +12,6 @@ from slack_sdk import WebhookClient
 
 from radiko_timeshift_recorder.radiko import DateAreaSchedule, Program
 from radiko_timeshift_recorder.rules import Rules
-from radiko_timeshift_recorder.util import download
 
 
 def program_to_filename(program: Program) -> str:
@@ -24,6 +25,62 @@ def program_to_filename(program: Program) -> str:
         )
         + ".mp4"
     )
+
+
+def get_duration(filepath: Path) -> float:
+    proc = subprocess.run(
+        [
+            "ffprobe",
+            "-hide_banner",
+            "-show_streams",
+            "-print_format",
+            "json",
+            str(filepath.resolve()),
+        ],
+        capture_output=True,
+        text=True,
+    )
+    return float(json.loads(proc.stdout)["streams"][0]["duration"])
+
+
+def download(program: Program, out_filepath: Path) -> None:
+    part_filepath = out_filepath.with_suffix(out_filepath.suffix + ".part")
+
+    # TODO: avoid using subprocess and use streamlink API
+    # TODO: avoid using pipe
+    # TODO: avoid using ffmpeg if possible
+    subprocess.run(
+        args=" ".join(
+            [
+                "python",
+                "-m",
+                "streamlink",
+                program.url,
+                "best",
+                "-O",
+                "|",
+                "ffmpeg",
+                "-i",
+                "-",
+                "-c",
+                "copy",
+                "-f",
+                "mp4",
+                "-y",
+                f'"{part_filepath}"',
+            ]
+        ),
+        shell=True,
+    )
+
+    recorded_dur = get_duration(part_filepath)
+
+    if abs(recorded_dur - program.dur) > 1:
+        raise AssertionError(
+            f"Recorded duration {recorded_dur} differs from the program duration {program.dur}."
+        )
+
+    part_filepath.replace(out_filepath)
 
 
 @click.command()
