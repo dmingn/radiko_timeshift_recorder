141097a3343b5ed86da0257a07562c0e516c9b38
dmingn <dmingn@users.noreply.github.com>
2022-04-12 00:15:46 +0900

    check recorded duration (#8)

---
+++ radiko_timeshift_recorder/util.py
@@ -0,0 +1,61 @@
+import json
+import subprocess
+from pathlib import Path
+
+from radiko_timeshift_recorder.radiko import Program
+
+
+def get_duration(filepath: Path) -> float:
+    proc = subprocess.run(
+        [
+            "ffprobe",
+            "-hide_banner",
+            "-show_streams",
+            "-print_format",
+            "json",
+            str(filepath.resolve()),
+        ],
+        capture_output=True,
+        text=True,
+    )
+    return float(json.loads(proc.stdout)["streams"][0]["duration"])
+
+
+def download(program: Program, out_filepath: Path) -> None:
+    part_filepath = out_filepath.with_suffix(out_filepath.suffix + ".part")
+
+    # TODO: avoid using subprocess and use streamlink API
+    # TODO: avoid using pipe
+    # TODO: avoid using ffmpeg if possible
+    subprocess.run(
+        args=" ".join(
+            [
+                "python",
+                "-m",
+                "streamlink",
+                program.url,
+                "best",
+                "-O",
+                "|",
+                "ffmpeg",
+                "-i",
+                "-",
+                "-c",
+                "copy",
+                "-f",
+                "mp4",
+                "-y",
+                f'"{part_filepath}"',
+            ]
+        ),
+        shell=True,
+    )
+
+    recorded_dur = get_duration(part_filepath)
+
+    if abs(recorded_dur - program.dur) > 1:
+        raise AssertionError(
+            f"Recorded duration {recorded_dur} differs from the program duration {program.dur}."
+        )
+
+    part_filepath.replace(out_filepath)
